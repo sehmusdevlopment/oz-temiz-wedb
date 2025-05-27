e04f6058ce5abf2f4b2dcd39fd0ba3f146f6a9e3
şehmus <sehmustemiz56@gmail.com>
2025-05-27 15:53:31 +0300

    öztemizweb

---
+++ script.js
@@ -0,0 +1,7 @@
+// Basit mobil menü toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const navUl = document.querySelector('nav ul');
+
+menuToggle?.addEventListener('click', () => {
+  navUl.classList.toggle('active');
+});
